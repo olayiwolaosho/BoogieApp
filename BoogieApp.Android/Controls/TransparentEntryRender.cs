162fffbe62ddec68de875553806e6be080c0f6e7
Cokeroluwasegun <cokeroluwasegun@outlook.com>
2020-05-26 16:30:06 +0100

    Custom Entry

---
+++ BoogieApp.Android/Controls/TransparentEntryRender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Text;
+using BoogieApp.Droid.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BoogieApp.Controls.TransparentEntry), typeof(TransparentEntryRender))]
+namespace BoogieApp.Droid.Controls
+{
+    public class TransparentEntryRender : EntryRenderer
+    {
+        public TransparentEntryRender(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                if (e.OldElement == null)
+                {
+                    //Control.SetBackgroundResource(Resource.Layout.rounded_shape);
+                    var gradientDrawable = new GradientDrawable();
+                    gradientDrawable.SetCornerRadius(60f);
+                   // gradientDrawable.SetStroke(5, Android.Graphics.Color.DeepPink);
+                    gradientDrawable.SetColor(Android.Graphics.Color.White);
+                    Control.SetBackground(gradientDrawable);
+
+                    Control.SetPadding(50, Control.PaddingTop, Control.PaddingRight,
+                        Control.PaddingBottom);
+                }
+            }
+        }
+    }
+}
---
+++ BoogieApp/Model/LoginpageModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BoogieApp.Model
+{
+    public class LoginpageModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
