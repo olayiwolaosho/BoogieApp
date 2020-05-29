bec3e839cb66d883529f30585dbcd839f816e84b
Cokeroluwasegun <cokeroluwasegun@outlook.com>
2020-05-29 12:46:14 +0100

    New commit for LoginPage and Map modification

---
+++ BoogieApp/Views/MapPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace BoogieApp.Views
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+            Map map = new Map();
+            Content = map;
+        }
+    }
+}
